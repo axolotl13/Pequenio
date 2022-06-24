580af2bda2514e5228a3a4c611895c213a172ed7
pepecr4ft <geovannicinto@gmail.com>
2022-06-24 02:18:58 -0500

    mejorando indent

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -3,15 +3,18 @@ if not ok then return end
 
 local opts = {
   char = "▏",
+  context_char = "▏",
   -- char_list = {'|', '¦', '┆', '┊'},
+  max_indent_increase = 1,
   show_current_context = true,
   show_current_context_start = true,
   show_trailing_blankline_indent = false,
   -- show_end_of_line = true,
   use_treesitter = true,
-  buftype_exclude = {"terminal", "help", "packes", "TelescopePrompt", "TelescopeResults", "lspinfo"},
+  buftype_exclude = {"terminal", "help", "packes", "TelescopePrompt", "TelescopeResults"},
   filetype_exclude = {
-    "alpha", "help", "terminal", "packer", "sh", "markdown", "md", "txt", "css", "lsp-installer"
+    "alpha", "help", "terminal", "packer", "sh", "markdown", "md", "txt", "css", "lsp-installer",
+    "lspinfo"
   },
   context_patterns = {
     "class", "function", "method", "try", "^if", "^while", "^for", "^switch", "^object", "^table",
