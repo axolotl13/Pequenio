41477850673c10590aa55c3abf3538e3c3da3272
pepecr4ft <geovannicinto@gmail.com>
2022-06-24 00:44:26 -0500

    formateo de nvimtree

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -8,35 +8,18 @@ local opts = {
   ignore_buffer_on_setup = false,
   update_cwd = true,
   respect_buf_cwd = true,
-  view = {
-    width = 32,
-    height = 30,
-    mappings = {
-      custom_only = false,
-      list = {},
-    },
-  },
+  view = {width = 32, height = 30, mappings = {custom_only = false, list = {}}},
   renderer = {
     group_empty = true,
     highlight_git = true,
-    root_folder_modifier = table.concat{":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"},
+    root_folder_modifier = table.concat {":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??"},
     indent_markers = {
       enable = false,
-      icons = {
-        corner ="└ ",
-        edge = "│ ",
-        item = "│ ",
-        none = "  ",
-      },
+      icons = {corner = "└ ", edge = "│ ", item = "│ ", none = "  "}
     },
     icons = {
       symlink_arrow = icons.symlink_arrow,
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = false,
-        git = true,
-      },
+      show = {file = true, folder = true, folder_arrow = false, git = true},
       glyphs = {
         default = icons.default,
         symlink = icons.symlink,
@@ -48,7 +31,7 @@ local opts = {
           empty = icons.folder.empty,
           empty_open = icons.folder.empty_open,
           symlink = icons.folder.symlink,
-          symlink_open = icons.folder.symlink_open,
+          symlink_open = icons.folder.symlink_open
         },
         git = {
           unstaged = icons.git.unstaged,
@@ -57,22 +40,15 @@ local opts = {
           renamed = icons.git.renamed,
           untracked = icons.git.untracked,
           deleted = icons.git.deleted,
-          ignored = icons.git.ignored,
-        },
-      },
+          ignored = icons.git.ignored
+        }
+      }
     },
-    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    special_files = {"Cargo.toml", "Makefile", "README.md", "readme.md"}
   },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
+  update_focused_file = {enable = true, update_cwd = true, ignore_list = {}},
   ignore_ft_on_setup = {},
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
+  system_open = {cmd = nil, args = {}},
   diagnostics = {
     enable = true,
     show_on_dirs = false,
@@ -80,47 +56,35 @@ local opts = {
       hint = icons.diagnostics.hint,
       info = icons.diagnostics.info,
       warning = icons.diagnostics.info,
-      error = icons.diagnostics.info,
-    },
+      error = icons.diagnostics.info
+    }
   },
   filters = {
     dotfiles = false,
-    custom = {".git","node_modules", ".cache"},
-    exclude = {".gitignore", "gitsigns"},
+    custom = {".git", "node_modules", ".cache"},
+    exclude = {".gitignore", "gitsigns"}
   },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
+  git = {enable = true, ignore = true, timeout = 500},
   actions = {
     use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = true,
-      restrict_above_cwd = false,
-    },
+    change_dir = {enable = true, global = true, restrict_above_cwd = false},
     open_file = {
       quit_on_open = false,
       window_picker = {
         exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", "toggleterm" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
+          filetype = {"notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", "toggleterm"},
+          buftype = {"nofile", "terminal", "help"}
+        }
+      }
     },
-    remove_file = {
-      close_window = true,
-    },
+    remove_file = {close_window = true}
   },
-  trash = {
-    require_confirm = false,
-  },
+  trash = {require_confirm = false}
 }
 
 nvimtree.setup(opts)
 
 vim.api.nvim_create_autocmd('BufEnter', {
   command = [[if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]],
-  nested = true,
+  nested = true
 })
